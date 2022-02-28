dea71529687c851b12fb0107c831113d5b87cb25
ChaoJung <b93613029@ntu.edu.tw>
2022-02-28 22:50:05 +0800

    add: showing the times of guessing

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,9 @@
 #Let users guess a randomly generated number between 1 to 100, and give them tips (larger or smaller) to help them to find out the right number
 import random
 num = random.randint(1,100)
+count = 0
 while True:
+	count = count + 1
 	num_guess = int(input('猜數字，請輸入一個1-100之間的數字：'))
 	if num_guess > 100 or num_guess < 1:
 		print('要猜1-100之間的數字喔')
@@ -10,5 +12,7 @@ while True:
 		break
 	elif num > num_guess:
 		print('太小了，再往上猜！')
+		print('你已經猜了', count, '次了')
 	else:
 		print('太大了，請往下猜！')
+		print('你已經猜了', count, '次了')
